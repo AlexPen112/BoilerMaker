35c9b5d7c532de6ef04cacc172a0015d14249a4f
Alex Penaloza <alexjavier112@gmail.com>
2019-10-28 19:27:29 -0400

    react issues

---
+++ server/index.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const express = require("express");
+const app = express();
+const PORT = 8080
+
+app.use('/api', require('./api'))
+
+app.use(express.static(path.join(__dirname, "../public")));
+
+app.use((req, res, next) => {
+    if (path.extname(req.path).length) {
+      const err = new Error('Not found')
+      err.status = 404
+      next(err)
+    } else {
+      next()
+    }
+  })
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(err.status || 500).send(err.message || "Internal server error");
+});
+
+db.sync() // if you update your db schemas, make sure you drop the tables first and then recreate them
+  .then(() => {
+    console.log('db synced')
+    app.listen(PORT, () => console.log(`studiously serving silly sounds on port ${PORT}`))
+  })
